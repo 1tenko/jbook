1ee78f23215f4c2422f36f7c6391e1e0e80c1de9
tenko <tajm.ogunbiyi@gmail.com>
2022-07-29 00:29:16 +0100

    :construction_worker: can now live fetch a file from unpkg and include it in a bundle (dynamically fetching modules)

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -1,14 +1,25 @@
 import * as esbuild from "esbuild-wasm";
+import axios from "axios";
 
 export const unpkgPathPlugin = () => {
   return {
     name: "unpkg-path-plugin",
+    // build represents the bundling process
     setup(build: esbuild.PluginBuild) {
+      // if there are any import/require/exports, figure out where the requested file is
       build.onResolve({ filter: /.*/ }, async (args: any) => {
-        console.log("onResole", args);
-        return { path: args.path, namespace: "a" };
+        console.log("onResolve", args);
+        if (args.path === "index.js") {
+          return { path: args.path, namespace: "a" };
+        } else if (args.path === "tiny-test-pkg") {
+          return {
+            path: "https://unpkg.com/tiny-test-pkg@1.0.0/index.js",
+            namespace: "a",
+          };
+        }
       });
 
+      // attempt to load up that file
       build.onLoad({ filter: /.*/ }, async (args: any) => {
         console.log("onLoad", args);
 
@@ -16,16 +27,17 @@ export const unpkgPathPlugin = () => {
           return {
             loader: "jsx",
             contents: `
-              import message from './message';
+              const message = require('tiny-test-pkg');
               console.log(message);
             `,
           };
-        } else {
-          return {
-            loader: "jsx",
-            contents: 'export default "hi there!"',
-          };
         }
+
+        const { data } = await axios.get(args.path);
+        return {
+          loader: "jsx",
+          contents: data,
+        };
       });
     },
   };
