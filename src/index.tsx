096523aa76d65ade9b4b67e9a1c8caf7874e5e52
tenko <tajm.ogunbiyi@gmail.com>
2022-07-28 18:29:28 +0100

    :construction_worker: basic form elements created

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,25 @@
+import { useState } from "react";
 import ReactDOM from "react-dom";
 
 const App = () => {
+  const [input, setInput] = useState("");
+  // output from ESBuild
+  const [code, setCode] = useState("");
+
+  const onClick = () => {
+    console.log(input);
+  };
+
   return (
     <div>
-      <h1>hi</h1>
+      <textarea
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      ></textarea>
+      <div>
+        <button onClick={onClick}>Submit</button>
+      </div>
+      <pre>{code}</pre>
     </div>
   );
 };
