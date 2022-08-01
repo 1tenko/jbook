2db48c398626df9eba3167c97b2a363361cfd169
tenko <tajm.ogunbiyi@gmail.com>
2022-08-01 19:02:00 +0100

    :sparkles: passing code to iframe, safely handling untrusted code execution

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,8 @@ import { fetchPlugin } from './plugins/fetch-plugin';
 const App = () => {
   const ref = useRef<any>();
 
+  const iframe = useRef<any>();
+
   const [input, setInput] = useState('');
   // output from ESBuild
   const [code, setCode] = useState('');
@@ -28,7 +30,7 @@ const App = () => {
 
   const onClick = async () => {
     try {
-      const res = await esbuild.build({
+      const result = await esbuild.build({
         entryPoints: ['index.js'],
         bundle: true,
         write: false,
@@ -39,17 +41,26 @@ const App = () => {
         },
       });
 
-      // console.log(res);
-      setCode(res.outputFiles[0].text);
+      // console.log(result);
+      // setCode(res.outputFiles[0].text);
+      iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*');
     } catch (error) {
       console.error(error);
     }
   };
 
   const html = `
-    <script>
-    ${code}
-    </script>
+    <html>
+      <head></head>
+      <body>
+        <div id="root"></div>
+        <script>
+          window.addEventListener('message', (event) => {
+            eval(event.data);
+          }, false);
+        </script>
+      </body>
+    </html>
   `;
 
   return (
@@ -62,7 +73,12 @@ const App = () => {
         <button onClick={onClick}>Submit</button>
       </div>
       <pre>{code}</pre>
-      <iframe title="codeOutput" sandbox="allow-scripts" srcDoc={html} />
+      <iframe
+        ref={iframe}
+        title="codeOutput"
+        sandbox="allow-scripts"
+        srcDoc={html}
+      />
     </div>
   );
 };
