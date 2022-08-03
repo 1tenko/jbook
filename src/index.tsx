9fe53774c0a439644df06a08e13a82646acd071c
tenko <tajm.ogunbiyi@gmail.com>
2022-08-03 01:57:16 +0100

    :recycle: extracting bundler logic

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,9 @@
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
-import { useEffect, useState, useRef } from 'react';
+import { useState, useRef } from 'react';
 import ReactDOM from 'react-dom/client';
-import * as esbuild from 'esbuild-wasm';
-import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
-import { fetchPlugin } from './plugins/fetch-plugin';
 import CodeEditor from './components/code-editor';
 import Preview from './components/preview';
+import { bundle } from './bundler';
 
 const App = () => {
   const ref = useRef<any>();
@@ -14,38 +12,9 @@ const App = () => {
 
   const [input, setInput] = useState('');
 
-  const startService = async () => {
-    try {
-      ref.current = await esbuild.initialize({
-        worker: true,
-        wasmURL: 'https://unpkg.com/esbuild-wasm@0.14.51/esbuild.wasm',
-      });
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  useEffect(() => {
-    startService();
-  }, []);
-
   const onClick = async () => {
-    try {
-      const result = await esbuild.build({
-        entryPoints: ['index.js'],
-        bundle: true,
-        write: false,
-        plugins: [unpkgPathPlugin(), fetchPlugin(input)],
-        define: {
-          'process.env.NODE_ENV': "'production'",
-          global: 'window',
-        },
-      });
-
-      setCode(result.outputFiles[0].text);
-    } catch (error) {
-      console.error(error);
-    }
+    const output = await bundle(input);
+    setCode(output);
   };
 
   return (
